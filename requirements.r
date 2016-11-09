40834d74a3c4a8aad9a97665404c420ea1ec0194
Vincent Vankrunkelsven <vincent@datacamp.com>
2016-11-09 17:03:38 +0100

    Update requirements.r

--- requirements.r
+++ requirements.r
@@ -1,2 +1,2 @@
-install.packages('dplyr', repos='http://cran.us.r-project.org')
-install.packages('MindOnStats', repos='http://cran.us.r-project.org')
+install.packages('dplyr')
+install.packages('MindOnStats')
